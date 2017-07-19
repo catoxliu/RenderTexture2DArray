39f519286e6be853cc799596f2623ce19bab5331
liushijian <harryliu_2336@163.com>
2017-07-19 16:05:23 +0800

    Issue: add RT2DARRAY.GL.LoadOrtho(Material) instead of normal GL.LoadOrtho() in single pass stereo rendering mode.

---
+++ Assets/RenderTexture2DArray/Examples/Scripts/RT2DPostRender.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RT2DPostRender : MonoBehaviour {
+
+    private Material fadeMaterial = null;
+
+    void Awake()
+    {
+        fadeMaterial = new Material(Shader.Find("Unlit/Color"));
+    }
+
+    void OnPostRender()
+    {
+        fadeMaterial.SetPass(0);
+        GL.PushMatrix();
+        //GL.LoadOrtho();
+        //Use below instead!
+        RT2DARRAY.GL.LoadOrtho(fadeMaterial);
+
+        GL.Color(fadeMaterial.color);
+        GL.Begin(GL.QUADS);
+        GL.Vertex3(0.4f, 0.4f, -3);
+        GL.Vertex3(0.4f, 0.6f, -3);
+        GL.Vertex3(0.6f, 0.6f, -3);
+        GL.Vertex3(0.6f, 0.4f, -3);
+        GL.End();
+        GL.Flush();
+        GL.PopMatrix();
+    }
+
+}
---
+++ Assets/RenderTexture2DArray/Scripts/GL.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace RT2DARRAY
+{
+    public sealed class GL
+    {
+        public static void LoadOrtho(Material mat)
+        {
+            Matrix4x4 proj = Matrix4x4.Ortho(0, 1, 0, 1, 0.01f, 100);
+            proj = UnityEngine.GL.GetGPUProjectionMatrix(proj, false);
+            UnityEngine.GL.LoadIdentity();
+            UnityEngine.GL.LoadProjectionMatrix(proj);
+
+            MultipleMatrix("unity_CameraProjection", "unity_StereoCameraProjection", mat);
+            MultipleMatrix("unity_CameraInvProjection", "unity_StereoCameraInvProjection", mat);
+            MultipleMatrix("glstate_matrix_projection", "unity_StereoMatrixP", mat);
+            MultipleMatrix("unity_MatrixVP", "unity_StereoMatrixVP", mat);
+            MultipleMatrix("unity_MatrixInvV", "unity_StereoMatrixInvV", mat);
+            MultipleMatrix("unity_MatrixV", "unity_StereoMatrixV", mat);
+            MultipleMatrix("unity_WorldToCamera", "unity_StereoWorldToCamera", mat);
+            MultipleMatrix("unity_CameraToWorld", "unity_StereoCameraToWorld", mat);
+            MultipleMatrix("_WorldSpaceCameraPos", "unity_StereoWorldSpaceCameraPos", mat);
+        }
+
+        static void MultipleMatrix(string shader_name, string target_name, Material mat)
+        {
+            Matrix4x4 tmp = Shader.GetGlobalMatrix(shader_name);
+            if (tmp == null)
+            {
+                Debug.LogError("No shader name " + shader_name);
+                return;
+            }
+            //Debug.Log(shader_name + tmp);
+            Matrix4x4[] result = new Matrix4x4[2];
+            result[1] = tmp;
+            result[0] = tmp;
+            mat.SetMatrixArray(target_name, result);
+        }
+    }
+}
